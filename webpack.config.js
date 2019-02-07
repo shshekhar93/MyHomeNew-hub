75c2f38e127557945f82f8ae5879ffb4242cfdf6
Shashi Shekhar <shashi20008@gmail.com>
2019-02-07 15:56:39 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  watch: false,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};

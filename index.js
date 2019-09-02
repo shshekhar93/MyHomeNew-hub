83dcb32722a1412bdacfe3758ba2c3e8b9e60f07
Shashi Shekhar <shashi20008@gmail.com>
2019-09-02 22:12:38 +0530

    add sqlite3 sessoin store

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
 const passport = require('passport');
 const morgan = require('morgan');
 
@@ -14,12 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static('./dist'));
+app.use(express.static('./public'));
 app.use(morgan('tiny'));
 
 app.use(session({ 
     secret: 'keyboard cat',
     saveUninitialized: true,
-    resave: true
+    resave: true,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
+    store: new SQLiteStore
 }));
 
 app.use(passport.initialize());

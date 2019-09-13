29af92a410fa60690e95e4a2e93683c57a429b14
Shashi Shekhar <shashi20008@gmail.com>
2019-09-13 20:45:15 +0530

    trying dual dns now

--- libs/dnssd.js
+++ libs/dnssd.js
@@ -5,36 +5,68 @@ const _get = require('lodash/get');
 const dns = require('dns');
 const util = require('util');
 const lookup = util.promisify(dns.lookup);
-const dnssd = require('dnssd');
+
+function getDNSLib() {
+  if(require('is-wsl')) {
+    const dnssd = require('dnssd');
+
+    dnssd.createBrowser = function(type, options) {
+      return new dnssd.Browser(type, options);
+    };
+
+    const origResolve = dnssd.resolve;
+    dnssd.resolve = function(name, options) {
+      return origResolve.apply(dnssd, [name, 'A', options]);
+    }
+
+    return dnssd;
+  }
+  
+  const mdns = require('mdns');
+  mdns.Browser.defaultResolverSequence[1] = 'DNSServiceGetAddrInfo' in mdns.dns_sd ? 
+    mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({families:[4]});
+  mdns.resolve = util.promisify(mdns.resolve.bind(mdns));
+  return mdns;
+}
+const mDNSLib = getDNSLib();
+
 const devices = {};
 
 try {
-  dnssd.Browser(dnssd.tcp('http'))
-    .on('serviceUp', function(service) {
-      const {
-        name = '', host = '', port = 80, addresses = []
-      } = service;
-      const ip = addresses.find(address => (address || '').split('.').length === 4);
+  const browser = mDNSLib.createBrowser(mDNSLib.tcp('http'));
+  browser.on('serviceUp', function(service) {
+    const {
+      name = '', host = '', port = 80, txt ={}, addresses = []
+    } = service;
+    const ip = addresses.find(address => (address || '').split('.').length === 4);
 
-      if(name.startsWith('myhomenew') && host && ip) {
-        devices[name] = { host, ip, port };
-        console.log('found', devices[name]);
+    if(name.startsWith('myhomenew') && host && ip) {
+      devices[name] = { host, ip, port, type: _get(txt, 'type') };
+      if(!devices[name].type) {
+        devices[name].type = 'switch';
       }
-    })
-    .on('serviceDown', function(service) {
-      const { name = '' } = service;
+      console.log('found', devices[name]);
+    }
+  });
+  browser.on('serviceDown', function(service) {
+    const { name = '' } = service;
 
-      if(devices[name]) {
-        console.log('lost', devices[name]);
-        devices[name] = undefined;
-      }
-    })
-    .on('error', function(err) {
-      console.error(err);
-    }).start();
+    if(devices[name]) {
+      console.log('lost', devices[name]);
+      devices[name] = undefined;
+    }
+  });
+  browser.on('error', function(err) {
+    console.error(err);
+
+    // restart browsing..
+    browser.stop();
+    window.setTimeout(() => browser.start(), 2000);
+  });
+  browser.start();
     
 } catch(e) {
-  console.error('could not create browser.')
+  console.error('could not create browser.', e.stack || e);
 }
 
 module.exports.getKnownDevices = () => _cloneDeep(devices);
@@ -43,7 +75,7 @@ module.exports.resolve = (name) => {
     return Promise.resolve(devices[name].ip);
   }
   
-  return dnssd.resolve(`${name}.local.`, 'A')
-    .then(result => console.log('miss') || _get(result, 'answer.address'))
+  return mDNSLib.resolve(`${name}.local`)
+    .then(result => _get(result, 'answer.address'))
     .catch(() => lookup(`${name}.local`, { family: 4 }));
 };

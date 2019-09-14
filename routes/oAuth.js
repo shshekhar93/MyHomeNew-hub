7d768a106bee673000bbc16999b1668f5ccb1ce1
Shashi Shekhar <shashi20008@gmail.com>
2019-09-14 20:47:46 +0530

    Update oAuth templates to be responsive

--- routes/oAuth.js
+++ routes/oAuth.js
@@ -7,12 +7,26 @@ module.exports = (app) => {
       return res.redirect(`/login?redirectTo=${encodeURIComponent(req.originalUrl)}`)
     }
     
-    const prefix = '<!DOCTYPE html><html><head><title>Authorize</title></head><body>' +
-      '<form action="/authorize" method="POST"><input type="hidden" name="grant_type" value="code" />' +
-      '<h4>Authorize Google assistant to control your devices</h4>';
+    const prefix = `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>Authorize</title>
+          <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap.min.css" crossorigin="anonymous" />
+        </head>
+        <body>
+          <div class="container" style="padding-top: 50px; text-align: center">
+            <form action="/authorize" method="POST">
+              <input type="hidden" name="grant_type" value="code" />
+              <h4>Authorize Google assistant to control your devices</h4>`;
+    const postfix = `
+              <button class="btn btn-primary" type="submit" style="margin-top: 30px;">Authorize</button>
+            </form>
+          </div>
+        </body>
+      </html>`;
     const inputs = Object.keys(req.query).reduce((html, key) => 
       html + `<input type="hidden" name="${key}" value="${req.query[key]}" />`, '');
-    const postfix = '<button type="submit">Authorize</button></form></body></html>'
     res.type('html').send(prefix + inputs + postfix);
   });
   app.post('/authorize', authorize, (req, res, next) => console.log('hrer') || next(), app.oAuth.authorize({

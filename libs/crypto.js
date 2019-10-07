b7e7ab7b4391599af3ba0e8c2e728ee04110ac34
Shashi Shekhar <shashi20008@gmail.com>
2019-10-08 00:28:18 +0530

    User CTR instead of CBC

--- libs/crypto.js
+++ libs/crypto.js
@@ -19,9 +19,14 @@ function randomBytes(len, encoding) {
  * @param {String} key : Hex encoded key
  */
 function encrypt(plainText, key) {
+  // const paddingLen = plainText.length % 16 === 0 ? 0 : (16 - (plainText.length % 16));
+  // if(paddingLen) {
+  //   plainText = plainText + Array(paddingLen).fill(' ').join('');
+  // }
+  
   const secret = Buffer.from(key, 'hex');
   const IV = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-128-cbc', Buffer.from(secret), IV);
+  const cipher = crypto.createCipheriv('aes-128-ctr', secret, IV);
   let encrypted = cipher.update(plainText);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   return IV.toString('hex') + '-' + encrypted.toString('hex');
@@ -31,9 +36,8 @@ function decrypt(cipherText, key, encoding) {
   let [iv, encrypted] = cipherText.split('-');
   iv = Buffer.from(iv, 'hex');
   encrypted = Buffer.from(encrypted, 'hex');
-
   const secret = Buffer.from(key, 'hex');
-  const deCipher = crypto.createDecipheriv('aes-128-cbc', secret, iv);
+  const deCipher = crypto.createDecipheriv('aes-128-ctr', secret, iv);
   let decrypted = deCipher.update(encrypted);
   decrypted = Buffer.concat([decrypted, deCipher.final()]);
   return encoding ? decrypted.toString(encoding) : decrypted;
